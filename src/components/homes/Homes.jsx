aeb08b021ce52cf4877f3d6d21193f72fd239d9e
Ellina Babaian <famogl375@gmail.com>
2023-09-04 22:24:36 -0700

    Adding svgs

--- src/components/homes/Homes.jsx
+++ src/components/homes/Homes.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import "./Homes.css"
+import "./Homes.css";
+import {
+  CubeIcon,
+  ExpandIcon,
+  HeartIcon,
+  MapIcon,
+  MoneyIcon,
+} from "../svg/Svg";
 
 const Homes = () => {
   return (
     <section className="homes">
       <div className="home">
         <img src="img/house1.jpg" alt="House 1" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Beautiful Family House</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>USA</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>5 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             325 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$1,200,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
@@ -37,30 +38,24 @@ const Homes = () => {
 
       <div className="home">
         <img src="img/house2.jpg" alt="House 2" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Modern Glass Villa</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>Canada</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>6 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             450 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$2,750,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
@@ -68,30 +63,24 @@ const Homes = () => {
 
       <div className="home">
         <img src="img/house3.jpg" alt="House 3" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Cozy Country House</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>UK</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>4 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             250 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$850,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
@@ -99,30 +88,24 @@ const Homes = () => {
 
       <div className="home">
         <img src="img/house4.jpg" alt="House 4" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Large Rustical Villa</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>Portugal</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>6 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             480 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$1,950,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
@@ -130,30 +113,24 @@ const Homes = () => {
 
       <div className="home">
         <img src="img/house5.jpg" alt="House 5" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Majestic Palace House</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>Germany</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>18 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             4230 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$9,500,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
@@ -161,30 +138,24 @@ const Homes = () => {
 
       <div className="home">
         <img src="img/house6.jpg" alt="House 6" className="home__img" />
-        <svg className="home__like">
-          {/* <use xlink:href="img/sprite.svg#icon-heart-full"></use> */}
-        </svg>
+        {/* <HeartIcon /> */}
         <h5 className="home__name">Modern Family Apartment</h5>
         <div className="home__location">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-map-pin"></use> */}
-          </svg>
+          <MapIcon />
           <p>Italy</p>
         </div>
         <div className="home__rooms">
-          <svg>
-            {/* <use xlink:href="img/sprite.svg#icon-profile-male"></use> */}
-          </svg>
+          <CubeIcon />
           <p>3 rooms</p>
         </div>
         <div className="home__area">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-expand"></use> */}</svg>
+          <ExpandIcon />
           <p>
             180 m<sup>2</sup>
           </p>
         </div>
         <div className="home__price">
-          <svg>{/* <use xlink:href="img/sprite.svg#icon-key"></use> */}</svg>
+          <MoneyIcon />
           <p>$600,000</p>
         </div>
         <button className="btn home__btn">Contact realtor</button>
---
+++ src/components/svg/Svg.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+export const HeartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-heart-fill home__like home__like"
+      viewBox="0 0 16 16"
+    >
+      {" "}
+      <path
+        fill-rule="evenodd"
+        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+      />{" "}
+    </svg>
+  );
+};
+
+export const MapIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-pin-map-fill"
+      viewBox="0 0 16 16"
+    >
+      {" "}
+      <path
+        fill-rule="evenodd"
+        d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"
+      />{" "}
+      <path
+        fill-rule="evenodd"
+        d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"
+      />{" "}
+    </svg>
+  );
+};
+
+export const CubeIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+      <rect width="256" height="256" fill="none" />
+      <path d="M229.9,70.8h0a.1.1,0,0,1-.1-.1,16.2,16.2,0,0,0-6-5.9l-88-49.5a16,16,0,0,0-15.6,0l-88,49.5a16.2,16.2,0,0,0-6,5.9.1.1,0,0,1-.1.1v.2A15,15,0,0,0,24,78.7v98.6a16.1,16.1,0,0,0,8.2,14l88,49.5a16.5,16.5,0,0,0,7.2,2h1.4a16.1,16.1,0,0,0,7-2l88-49.5a16.1,16.1,0,0,0,8.2-14V78.7A15.6,15.6,0,0,0,229.9,70.8Zm-101,48L48.4,74,128,29.2,207.7,74ZM216,177.3l-79.9,45,.8-89.6,79.1-45Z" />
+    </svg>
+  );
+};
+
+export const MoneyIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      {" "}
+      <g>
+        {" "}
+        <path fill="none" d="M0 0h24v24H0z" />{" "}
+        <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-3.5-8v2H11v2h2v-2h1a2.5 2.5 0 1 0 0-5h-4a.5.5 0 1 1 0-1h5.5V8H13V6h-2v2h-1a2.5 2.5 0 0 0 0 5h4a.5.5 0 1 1 0 1H8.5z" />{" "}
+      </g>{" "}
+    </svg>
+  );
+};
+
+export const ExpandIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-arrows-angle-expand"
+      viewBox="0 0 16 16"
+    >
+      {" "}
+      <path
+        fill-rule="evenodd"
+        d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"
+      />{" "}
+    </svg>
+  );
+};
