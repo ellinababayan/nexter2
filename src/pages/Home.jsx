bfe2e63e2774a0c33413b82fb2918d65ff5bc613
Ellina Babaian <famogl375@gmail.com>
2023-09-04 21:30:30 -0700

    Gallery component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Features from "../components/features/Features";
 import Story from "../components/story/StoryPictures";
 import StoryContent from "../components/story/StoryContent";
 import Homes from "../components/homes/Homes";
+import Gallery from "../components/gallery/Gallery";
 
 const Home = () => {
   return (
@@ -17,108 +18,8 @@ const Home = () => {
       <Features />
       <Story />
       <StoryContent />
-
       <Homes />
-      <section className="gallery">
-        <figure className="gallery__item gallery__item--1">
-          <img
-            src="img/inside1.jpg"
-            alt="Gallery image 1"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--2">
-          <img
-            src="img/inside2.jpg"
-            alt="Gallery image 2"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--3">
-          <img
-            src="img/inside3.jfif"
-            alt="Gallery image 3"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--4">
-          <img
-            src="img/inside4.webp"
-            alt="Gallery image 4"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--5">
-          <img
-            src="img/inside5.webp"
-            alt="Gallery image 5"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--6">
-          <img
-            src="img/inside6.jpg"
-            alt="Gallery image 6"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--7">
-          <img
-            src="img/inside7.webp"
-            alt="Gallery image 7"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--8">
-          <img
-            src="img/inside8.webp"
-            alt="Gallery image 8"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--9">
-          <img
-            src="img/inside9.webp"
-            alt="Gallery image 9"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--10">
-          <img
-            src="img/inside10.jpg"
-            alt="Gallery image 10"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--11">
-          <img
-            src="img/inside11.jpg"
-            alt="Gallery image 11"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--12">
-          <img
-            src="img/inside12.png"
-            alt="Gallery image 12"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--13">
-          <img
-            src="img/inside13.webp"
-            alt="Gallery image 13"
-            className="gallery__img"
-          />
-        </figure>
-        <figure className="gallery__item gallery__item--14">
-          <img
-            src="img/inside14.jpg"
-            alt="Gallery image 14"
-            className="gallery__img"
-          />
-        </figure>
-      </section>
+      <Gallery />
 
       <footer className="footer">
         <ul className="nav">
