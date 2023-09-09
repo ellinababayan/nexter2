7d91c15643fe56864bbdce1f405781bb40703722
Ellina Babaian <famogl375@gmail.com>
2023-09-09 16:33:05 -0700

    Navabr refactoring #2

--- src/pages/navbar/NavbarPage.jsx
+++ src/pages/navbar/NavbarPage.jsx
@@ -37,23 +37,27 @@ import "./NavbarPage.css";
 const NavBarPage = (props) => {
   const isOpen = props.isOpen;
 
+  const handleClose = () => {
+    props.setIsOpen(false)
+  }
+
   return isOpen ? (
     <div className="navbar_page" id="navbar_page">
       <div className="navbar_page__content">
         <div className="navbar_parent">
-          <a href="#features" className="navbar_child">
+          <a href="#features" className="navbar_child" onClick={handleClose}>
             Features
           </a>
-          <a href="#story" className="navbar_child">
+          <a href="#story" className="navbar_child" onClick={handleClose}>
             Stories
           </a>
-          <a href="#houses" className="navbar_child">
+          <a href="#houses" className="navbar_child" onClick={handleClose}>
             Houses
           </a>
-          <a href="#gallery" className="navbar_child">
+          <a href="#gallery" className="navbar_child" onClick={handleClose}>
             Gallery
           </a>
-          <a href="#contact" className="navbar_child">
+          <a href="#contact" className="navbar_child" onClick={handleClose}>
             Contact Us
           </a>
         </div>
