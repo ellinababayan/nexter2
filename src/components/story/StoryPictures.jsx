059f61ae7bde56bd27b62b630ff6aa3ba1dd4d4d
Ellina Babaian <famogl375@gmail.com>
2023-09-09 19:42:30 -0700

    Story Pictures editing

--- src/components/story/StoryPictures.jsx
+++ src/components/story/StoryPictures.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import "./Story.css"
+import "./Story.css";
 
 const Story = () => {
   return (
-      <div className="story__pictures">
-        <img src="img/story-img--3 (2).jfif"  alt="Property" className="story__img--1" />
-        <img src="https://advantage4parents.com/wp-content/uploads/2017/11/happy-family.jpg" alt="Customers photo" className="story__img--2" />
-        {/* <!-- <img src="img/story-img--2.jpg" alt="" className="story__img--3"> --> */}
-      </div>
+    <div className="story__pictures">
+      {/* <img src="img/story-img--3 (2).jfif"  alt="Property" className="story__img--1" /> */}
+      <img
+        src="https://www.east-texas.com/images/photos-beaumont/mcfaddin-ward-house-museum-beaumont-texas.jpg"
+        alt="Property"
+        className="story__img--1"
+      />
+      <img
+        src="https://advantage4parents.com/wp-content/uploads/2017/11/happy-family.jpg"
+        alt="Customers photo"
+        className="story__img--2"
+      />
+    </div>
   );
 };
 
